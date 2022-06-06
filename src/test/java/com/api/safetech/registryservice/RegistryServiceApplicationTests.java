22b4e510e18428c0d3896910ddaa3cf61573c170
Debie <debiegarcia@hotmail.com>
2022-06-06 15:22:56 -0500

    Create Registry Service

---
+++ src/test/java/com/api/safetech/registryservice/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.safetech.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
